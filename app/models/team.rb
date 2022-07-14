2e8acede50d2e96f751d0b43ecd8b2177ebe7d05
James Robinson <james@robinjam.net>
2022-07-14 17:08:35 +0100

    Add basic framework for projects feature

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,6 +4,7 @@ class Team < ApplicationRecord
   has_many :regions, dependent: :destroy
   has_many :label_schemas, dependent: :destroy
   has_many :models, dependent: :destroy
+  has_many :projects, dependent: :destroy
 
   has_many :map_tile_layers, through: :regions
   has_many :overlays, through: :regions
