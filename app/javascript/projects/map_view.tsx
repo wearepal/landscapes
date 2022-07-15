90fb13aa126fbe30f6930a81468b74ea92afcbb7
James Robinson <james@robinjam.net>
2022-07-15 09:32:21 +0100

    Add zoom to extent control

--- app/javascript/projects/map_view.tsx
+++ app/javascript/projects/map_view.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react'
 
 import { Map, View } from 'ol'
+import { defaults as defaultControls, ZoomToExtent } from 'ol/control'
 import BaseLayer from 'ol/layer/Base'
+import { createEmpty as createEmptyExtent, extend, isEmpty } from 'ol/extent'
+
+function createIconElement(iconName: string) {
+  const result = document.createElement("i")
+  result.classList.add("fas", `fa-${iconName}`)
+  return result
+}
 
 interface MapViewProps {
   layers: BaseLayer[]
@@ -9,27 +17,51 @@ interface MapViewProps {
 export const MapView = ({ layers }: MapViewProps) => {
   const mapRef = React.useRef<HTMLDivElement>()
   const [map, setMap] = React.useState<Map | null>(null)
+  const [zoomToExtent, setZoomToExtent] = React.useState<ZoomToExtent | null>(null)
 
   React.useEffect(() => {
+    const zoomToExtent = new ZoomToExtent({ label: createIconElement("search-location") })
     const map = new Map({
       view: new View({
         center: [0, 0],
         zoom: 1
       }),
+      controls: defaultControls({
+        zoomOptions: {
+          zoomInLabel: createIconElement("search-plus"),
+          zoomOutLabel: createIconElement("search-minus")
+        }
+      }).extend([zoomToExtent]),
       target: mapRef.current
     });
 
     setMap(map)
+    setZoomToExtent(zoomToExtent)
 
     return () => {
       map.dispose()
       setMap(null)
+      setZoomToExtent(null)
     }
   }, [])
 
   React.useEffect(() => map?.setLayers(layers), [map, layers])
 
   React.useEffect(() => map?.updateSize())
+
+  React.useEffect(() => {
+    if (map !== null && zoomToExtent !== null) {
+      const extent = createEmptyExtent()
+      map.getLayers().forEach(layer => {
+        const layerExtent = layer.getExtent()
+        if (layerExtent !== undefined) {
+          extend(extent, layerExtent)
+        }
+      })
+
+      Object.assign(zoomToExtent, { extent: isEmpty(extent) ? undefined : extent })
+    }
+  })
 
   return <div className="flex-grow-1 bg-dark" ref={mapRef as any}></div>
 }
