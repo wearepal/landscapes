94bf93a02a99804123bd79faeee294de7f552770
James Robinson <james@robinjam.net>
2022-07-29 12:43:43 +0100

    Run CI system tests on Chrome & Firefox

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,5 +3,5 @@ require "test_helper"
 SimpleCov.command_name 'test:system'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: ENV.fetch("SYSTEM_TEST_BROWSER", "headless_chrome").to_sym, screen_size: [1400, 1400]
 end
