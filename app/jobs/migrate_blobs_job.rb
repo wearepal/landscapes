db20581b873e13cbe48246a6f4081418f928a5ef
James Robinson <james@robinjam.net>
2022-09-01 11:27:27 +0100

    Make MigrateBlobsJob ignore missing files

--- app/jobs/migrate_blobs_job.rb
+++ app/jobs/migrate_blobs_job.rb
@@ -6,11 +6,13 @@ class MigrateBlobsJob < ApplicationJob
     dst_service = ActiveStorage::Blob.services.fetch(to)
 
     ActiveStorage::Blob.where(service_name: src_service.name).find_each do |blob|
-      blob.open do |file|
-        dst_service.upload(blob.key, file, checksum: blob.checksum)
-      end
+      if src_service.exist? blob.key
+        blob.open do |file|
+          dst_service.upload(blob.key, file, checksum: blob.checksum)
+        end
 
-      blob.update! service_name: dst_service.name
+        blob.update! service_name: dst_service.name
+      end
     end
   end
 end
