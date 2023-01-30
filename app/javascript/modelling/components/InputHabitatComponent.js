e36d4f12156a5e8653924824c304d523a138772a
Foxino <foxino@users.noreply.github.com>
2023-01-30 11:35:13 +0000

    input habitat

--- app/javascript/modelling/components/InputHabitatComponent.js
+++ app/javascript/modelling/components/InputHabitatComponent.js
@@ -1,5 +1,13 @@
+import { zoom } from 'd3'
 import { Component, Output } from 'rete'
 import { setSocket } from '../sockets'
+
+import GeoJSON from 'ol/format/GeoJSON'
+import {createEmpty as createEmptyExtent, extend as extendExtent } from 'ol/extent'
+import { createXYZ } from 'ol/tilegrid'
+import { BooleanTileGrid } from '../TileGrid'
+import { couldStartTrivia } from 'typescript'
+import { array } from 'prop-types'
 
 
 export class InputHabitatComponent extends Component {
@@ -7,7 +15,7 @@ export class InputHabitatComponent extends Component {
     constructor() {
         super('Input habitat')
         this.category = 'Inputs & Outputs'
-        
+
         this.habitats = [
             //TODO : move to a json or an alternative storage
             {agg : 1, AC : 'Deciduous Woodland', mode: 1, LC : 'Deciduous Woodland'},
@@ -42,22 +50,115 @@ export class InputHabitatComponent extends Component {
             {agg : 10, AC : 'Built-up areas and gardens', mode: 21, LC : 'Suburban'},
         ]
     }
-
     
 
     builder(node) {
-
-        //create outputs for each type of habitat. 
 
         this.habitats.forEach((hab)=>{
             node.addOutput(new Output(hab['mode'], hab['LC'], setSocket))
         })
 
-    
+
+    }
+
+    async retrieveLandCoverData(bbox){
+
+        const response = await fetch('https://landscapes.wearepal.ai/geoserver/wfs?' + new URLSearchParams({
+        //const response = await fetch('http://localhost:8080/geoserver/wfs?' + new URLSearchParams({
+                outputFormat : 'application/json',
+            request : 'GetFeature',
+
+            //typeName = workspace:layer
+            typeName : 'ukceh:lcm_2021',
+            //typeName : 'ne:lcm_2021',
+
+            //hard coded bbox for now
+            bbox : bbox
+        },
+        {
+            crossDomain: true
+        }))
+
+        if (!response.ok) throw new Error()
+
+        return await response.json()
 
     }
 
     async worker(node, inputs, outputs) {
+
+        let bbox = '-14782.045007023014,6596819.173144216,-7883.035964960155,6612578.505519601,EPSG:3857'
+
+        let out = node.outputs;
+
+        const activeHabitats = this.habitats.filter(habitat => out[habitat.mode].connections.length > 0)
+
+
+        await this.retrieveLandCoverData(bbox).then((json)=>{
+
+
+            const features = new GeoJSON({dataProjection: json.crs?.properties?.name, featureProjection: "EPSG:3857"}).readFeatures(json)
+
+            const extent = features.reduce(
+                (extent, feature) => extendExtent(extent, feature.getGeometry().getExtent()),
+                createEmptyExtent()
+            )
+
+            let z = 18; //hard coded zoom for now.. 
+
+            const tileGrid = createXYZ()
+            const outputTileRange = tileGrid.getTileRangeForExtentAndZ(extent, z)
+
+
+            activeHabitats.forEach((ah)=>{
+                ah.result = new BooleanTileGrid(
+                    z,
+                    outputTileRange.minX, outputTileRange.minY,
+                    outputTileRange.getWidth(), outputTileRange.getHeight()
+                )
+            })
+
+            features.forEach(feature => {
+
+                const mode = feature.values_._mode
+
+                const index = activeHabitats.findIndex(x => x.mode == mode);
+
+                if(index != -1){
+
+                    const geom = feature.getGeometry()
+                    if(geom.getType() === "Point"){
+                        const [, x, y] = tileGrid.getTileCoordForCoordAndZ(geom.getCoordinates(), z)
+                        activeHabitats[index].result.set(x, y, true)
+                    }
+                    else{
+                        const featureTileRange = tileGrid.getTileRangeForExtentAndZ(geom.getExtent(), z)
+                        for(let x = featureTileRange.minX; x <= featureTileRange.maxX; ++x){
+                            for(let y = featureTileRange.minY; y <= featureTileRange.maxY; ++y){
+                                const tileExtent = tileGrid.getTileCoordExtent([z, x, y])
+                                if(geom.intersectsExtent(tileExtent)){
+                                    activeHabitats[index].result.set(x, y, true)
+                                }
+                            }
+                        }
+                    }
+
+                }
+
+
+
+            })
+
+            activeHabitats.forEach((ah)=>{
+
+                outputs[ah.mode] = ah.result
+                outputs[ah.mode].name = ah.LC
+
+            })
+
+        })
+
+
 
     }
 
