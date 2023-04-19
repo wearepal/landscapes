dd2e460adb8a07501cea93bbac856a1fd8bade58
= <pauljoncrossley@gmail.com>
2023-04-19 10:39:50 +0100

    ts

--- app/javascript/projects/modelling/components/digital_model_component.ts
+++ app/javascript/projects/modelling/components/digital_model_component.ts
@@ -1,6 +1,34 @@
-import { Node } from "rete";
+import { Node, Output } from "rete";
 import { NodeData, WorkerInputs, WorkerOutputs } from "rete/types/core/data";
 import { BaseComponent } from "./base_component";
+import { SelectControl } from "../controls/select";
+import { PreviewControl } from "../controls/preview";
+import { NumericTileGrid } from "../tile_grid";
+import { numericDataSocket } from "../socket_types";
+import { fromArrayBuffer } from "geotiff";
+import { createXYZ } from "ol/tilegrid";
+import { number } from "prop-types";
+
+interface DigitalModel {
+    id: number
+    name: string
+    source: string
+}
+
+const ModelList: Array<DigitalModel> = [
+    {
+        id: 0,
+        name: 'Digital Surface Model',
+        source: 'lidar:116807-4_DSM'
+    },
+    {
+        id: 1,
+        name: 'Digital Terrian Model',
+        source: 'lidar:116807-5_DTM'
+    }
+]
+
+const geoserver = "https://landscapes.wearepal.ai/geoserver/wms?"
 
 export class DigitalModelComponent extends BaseComponent {
 
@@ -10,10 +38,104 @@ export class DigitalModelComponent extends BaseComponent {
     }
 
     async builder(node: Node) {
+
+        node.addControl(
+            new SelectControl(
+                this.editor,
+                'sourceId',
+                () => ModelList,
+                () => [],
+                'Model'
+            )
+        )
+
+        node.addControl(new PreviewControl(() =>
+            node.meta.output as any || new NumericTileGrid(0, 0, 0, 1, 1)
+        ))
+
+        node.addOutput(new Output('dm', 'Output', numericDataSocket))
+
+
     }
 
 
+    async retrieveModelData(extent: any, source: string, tileRange: any) {
+
+        const [width, height] = [tileRange.getWidth(), tileRange.getHeight()]
+        const bbox = `${extent.join(",")},EPSG:3857`
+
+        const response = await fetch(
+            geoserver +
+            new URLSearchParams(
+                {
+                    service: 'WMS',
+                    version: '1.3.0',
+                    request: 'GetMap',
+                    layers: source,
+                    styles: '',
+                    format: 'image/geotiff',
+                    transparent: 'true',
+                    width,
+                    height,
+                    crs: 'EPSG:3857',
+                    bbox
+                }
+            )
+        )
+
+        const arrayBuffer = await response.arrayBuffer()
+        const tiff = await fromArrayBuffer(arrayBuffer)
+
+
+        return tiff
+
+    }
+
+
+
     async worker(node: NodeData, inputs: WorkerInputs, outputs: WorkerOutputs, ...args: unknown[]) {
+
+
+        const editorNode = this.editor?.nodes.find(n => n.id === node.id)
+        if (editorNode === undefined) { return }
+
+        let index = node.data.sourceId
+        if (index === undefined) { index = 0 }
+
+        let digitalModel = ModelList.find(a => a.id == index)?.source
+
+        if (digitalModel) {
+
+            const extent = [-20839.008676500813, 6579722.087031, 12889.487811, 6640614.986501137]
+
+            const zoom = 20
+
+            const tileGrid = createXYZ()
+            const outputTileRange = tileGrid.getTileRangeForExtentAndZ(extent, zoom)
+
+            const geotiff = await this.retrieveModelData(extent, digitalModel, outputTileRange)
+
+            const image = await geotiff.getImage()
+
+            const rasters = await geotiff.readRasters()
+
+            const out = editorNode.meta.output = outputs['dm'] = new NumericTileGrid(zoom, outputTileRange.minX, outputTileRange.minY, outputTileRange.getWidth(), outputTileRange.getHeight(), undefined)
+
+            for (let i = 0; i < rasters[0].length; i++) {
+
+                let y = (outputTileRange.minY + Math.floor(i / image.getWidth()))
+                let x = (outputTileRange.minX + i % image.getWidth())
+
+
+                out.set(x, y, rasters[0][i])
+
+            }
+
+            const previewControl: any = editorNode.controls.get('Preview')
+            previewControl.update()
+            editorNode.update()
+        }
+
     }
 
 }
