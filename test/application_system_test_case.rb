89b98e44498066f6057334fc45c99c6515a2e43e
Foxino <pauljoncrossley@gmail.com>
2023-08-15 10:32:30 +0100

    revert prev temp fix

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,8 +4,5 @@ SimpleCov.command_name 'test:system'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
 
-  # temporary fix until 115 has been released
-  Webdrivers::Chromedriver.required_version = '114.0.5735.90'
-
   driven_by :selenium, using: ENV.fetch("SYSTEM_TEST_BROWSER", "headless_chrome").to_sym, screen_size: [1400, 1400]
 end
