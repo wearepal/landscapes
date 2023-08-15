8e033151d11f636355ba25035e1e9a9ff14bd9df
Foxino <pauljoncrossley@gmail.com>
2023-08-15 11:33:08 +0100

    dynamic version retrieval

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,5 +4,7 @@ SimpleCov.command_name 'test:system'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
 
+  Webdrivers::Chromedriver.required_version = `curl https://chromedriver.storage.googleapis.com/LATEST_RELEASE`
+
   driven_by :selenium, using: ENV.fetch("SYSTEM_TEST_BROWSER", "headless_chrome").to_sym, screen_size: [1400, 1400]
 end
