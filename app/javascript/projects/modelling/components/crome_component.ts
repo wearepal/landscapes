a569901dae7b3bd2de2acfda1dba53742ed874c2
Novi Quadrianto <nq28@m900416.inf.susx.ac.uk>
2023-10-31 20:36:03 +0000

    still does not work with CROME

--- app/javascript/projects/modelling/components/crome_component.ts
+++ app/javascript/projects/modelling/components/crome_component.ts
@@ -11,25 +11,41 @@ import { BooleanTileGrid, CategoricalTileGrid } from "../tile_grid"
 const zoom = zoomLevel
 
 interface CropSpecies {
-  agg: number
-  LC: string
-  mode: number
-  LU: string
   LUCODE: string
+  LC: string
 }
 
 const cropspecies: CropSpecies[] = [
   //TODO : move to a json or an alternative storage
-  { agg: 1, LC: "Trees", mode: 1, LC: "Perennial Crops and Isolated Trees", LUCODE: "TC01" },
-  { agg: 1, LC: "Trees", mode: 2, LC: "Trees and Scrubs, short Woody plants, hedgerows", LUCODE: "WO12"},
-  { agg: 2, LC: "Grassland", mode: 3, LC: "Heathland and Bracken", LUCODE: "HE02"},
-  { agg: 2, LC: "Cereal Crops", mode: 4, LC: "Sunflower", LUCODE: "AC88"},
-  { agg: 3, LC: "Cereal Crops", mode: 5, LC: "Tomato", LUCODE: "AC45"},
-  { agg: 2, LC: "Cereal Crops", mode: 6, LC: "Potato", LUCODE: "AC44"},
-  { agg: 3, LC: "Leguminous Crops", mode: 7, LC: "Chickpea", LUCODE: "LG01"},
-  { agg: 3, LC: "Leguminous Crops", mode: 8, LC: "Soya", LUCODE: "LG08"},
-  { agg: 0, AC: "All", mode: 0, LC: "All" }
+  { LC: "Perennial Crops and Isolated Trees", LUCODE: "TC01" },
+  { LC: "Trees and Scrubs, short Woody plants, hedgerows", LUCODE: "WO12"},
+  { LC: "Heathland and Bracken", LUCODE: "HE02"},
+  { LC: "Sunflower", LUCODE: "AC88"},
+  { LC: "Tomato", LUCODE: "AC45"},
+  { LC: "Potato", LUCODE: "AC44"},
+  { LC: "Chickpea", LUCODE: "LG01"},
+  { LC: "Soya", LUCODE: "LG08"},
+  { LC: "All", LUCODE: "000"}
 ]
+
+async function fetchCROMEFromExtent(bbox: string, source: string) {
+
+    const response = await fetch(
+        "https://landscapes.wearepal.ai/geoserver/wfs?" +
+        new URLSearchParams(
+            {
+                outputFormat: 'application/json',
+                request: 'GetFeature',
+                typeName: source,
+                srsName: 'EPSG:3857',
+                bbox
+            }
+        )
+    )
+    if (!response.ok) throw new Error()
+
+    return await response.json()
+}
 
 async function renderCategoricalData() {
   // When testing locally, disable CORS in browser settings
@@ -37,16 +53,19 @@ async function renderCategoricalData() {
   const tileGrid = createXYZ()
   const outputTileRange = tileGrid.getTileRangeForExtentAndZ(extent, zoom)
 
-  const geotiff = await retrieveModelData(extent, 'crome:2021', outputTileRange)
+  //const geotiff = await retrieveModelData(extent, 'crome:2021', outputTileRange)
+  
+  //const rasters = await geotiff.readRasters()
+  //const image = await geotiff.getImage()
 
-  const rasters = await geotiff.readRasters()
-  const image = await geotiff.getImage()
-
+  const res = await fetchCROMEFromExtent(bbox, 'crome:crop_map_of_england_2020_east_sussex')
+  const features = new GeoJSON().readFeatures(res)
+  
 
   const map: Map<number, string> = new Map()
 
   cropspecies.forEach(crome => {
-    if (crome.mode !== 0) map.set(crome.mode, crome.LC)
+    if (crome.LUCODE !== "000") map.set(crome.LUCODE, crome.LC)
   })
 
   const result = new CategoricalTileGrid(
@@ -57,17 +76,47 @@ async function renderCategoricalData() {
     outputTileRange.getHeight()
   )
 
-  for (let i = 0; i < (rasters[0] as TypedArray).length; i++) {
+  //for (let i = 0; i < (rasters[0] as TypedArray).length; i++) {
 
-    let x = (outputTileRange.minX + i % image.getWidth())
-    let y = (outputTileRange.minY + Math.floor(i / image.getWidth()))
+  //  let x = (outputTileRange.minX + i % image.getWidth())
+  //  let y = (outputTileRange.minY + Math.floor(i / image.getWidth()))
 
-    result.set(x, y, rasters[0][i])
+  //  result.set(x, y, rasters[0][i])
 
+  //}
+
+  for (let feature of features){
+
+    const lucode = feature.get("lucode")
+    const index = cropspecies.findIndex(x => x.LUCODE === lucode)
+
+    if (index === -1) { continue }
+
+    const geom = feature.getGeometry()
+      const featureTileRange = tileGrid.getTileRangeForExtentAndZ(
+        geom.getExtent(),
+        zoom
+      )
+      for (
+        let x = Math.max(featureTileRange.minX, outputTileRange.minX);
+        x <= Math.min(featureTileRange.maxX, outputTileRange.maxX);
+        ++x
+      ) {
+        for (
+          let y = Math.max(featureTileRange.minY, outputTileRange.minY);
+          y <= Math.min(featureTileRange.maxY, outputTileRange.maxY);
+          ++y
+        ) {
+          const tileExtent = tileGrid.getTileCoordExtent([zoom, x, y])
+          if (geom.intersectsExtent(tileExtent)) {
+            result.set(x,y,lucode)
+          }
+        }
+      }
   }
-
+  
   result.setLabels(map)
-
+  
   return result
 }
 
@@ -89,37 +138,38 @@ export class CROMEComponent extends BaseComponent {
     node.meta.toolTipLink = "https://www.data.gov.uk/dataset/aaedb588-fc86-498f-acab-5fa1b261fdd5/crop-map-of-england-crome-2021"
 
     cropspecies.forEach(crome =>
-      crome.AC === "All" ? node.addOutput(new Output(crome["mode"].toString(), crome["LC"], categoricalDataSocket)) : node.addOutput(new Output(crome["mode"].toString(), crome["LC"], booleanDataSocket))
+      crome.LC === "All" ? node.addOutput(new Output(crome["LUCODE"], crome["LC"], categoricalDataSocket)) : node.addOutput(new Output(crome["LUCODE"], crome["LC"], booleanDataSocket))
     )
   }
 
   async worker(node: NodeData, inputs: WorkerInputs, outputs: WorkerOutputs, ...args: unknown[]) {
+    
     if (this.categoricalData === null) {
       this.categoricalData = await renderCategoricalData()
     }
     const categoricalData = this.categoricalData!
 
     cropspecies.filter(
-      crome => node.outputs[crome.mode].connections.length > 0
+      crome => node.outputs[crome.LUCODE].connections.length > 0
     ).forEach(crome => {
-      if (crome.mode === 0) {
+      if (crome.LUCODE === "000") {
 
-        outputs[crome.mode] = this.categoricalData
+        outputs[crome.LUCODE] = this.categoricalData
 
       } else {
-        if (this.outputCache.has(crome.mode)) {
-          outputs[crome.mode] = this.outputCache.get(crome.mode)
+        if (this.outputCache.has(crome.LUCODE)) {
+          outputs[crome.LUCODE] = this.outputCache.get(crome.LUCODE)
         }
         else {
-          const out = outputs[crome.mode] = new BooleanTileGrid(categoricalData.zoom, categoricalData.x, categoricalData.y, categoricalData.width, categoricalData.height)
+          const out = outputs[crome.LUCODE] = new BooleanTileGrid(categoricalData.zoom, categoricalData.x, categoricalData.y, categoricalData.width, categoricalData.height)
+          
           out.name = crome.LC
-
           for (let x = categoricalData.x; x < categoricalData.x + categoricalData.width; ++x) {
             for (let y = categoricalData.y; y < categoricalData.y + categoricalData.height; ++y) {
-              out.set(x, y, categoricalData.get(x, y) === crome.mode)
+              out.set(x, y, categoricalData.get(x, y) === crome.LUCODE)
             }
           }
-          this.outputCache.set(crome.mode, out)
+          this.outputCache.set(crome.LUCODE, out)
         }
       }
     })
