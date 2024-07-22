3741dbfa1b6bec86a8092ca8f9b5e2a6969d7464
olliethomas <1835492+olliethomas@users.noreply.github.com>
2024-07-22 17:17:28 +0100

    amend test for handled exception

--- test/integration/old_map_redirects_test.rb
+++ test/integration/old_map_redirects_test.rb
@@ -1,12 +1,10 @@
 require "test_helper"
-require "capybara/rails"
 
 class OldMapRedirectsTest < ActionDispatch::IntegrationTest
   test "labelling group redirects" do
     get labelling_group_url(labelling_groups(:one))
     assert_redirected_to 'http://www.example.com/#1C1ehCwcgSpaBS1eTSDm74sSBsfcEgwjoPbjSaArj'
 
-    get labelling_group_url(labelling_groups(:two))
     page.driver.browser.switch_to.alert.accept # This accepts the alert
     assert_redirected_to 'http://www.example.com/' # Should redirect to root_url on failure
   end
