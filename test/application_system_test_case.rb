80cb4458a4b94ce949376449fdd8568d75d5c7a4
Paul Crossley <pauljoncrossley@gmail.com>
2025-03-17 15:44:09 +0000

    CI Fixes

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,8 +3,10 @@ require "test_helper"
 SimpleCov.command_name 'test:system'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  
-  Webdrivers::Chromedriver.required_version = '114.0.5735.90'
-
-  driven_by :selenium, using: ENV.fetch("SYSTEM_TEST_BROWSER", "headless_chrome").to_sym, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome do |options|
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1400,1400')
+  end
 end
