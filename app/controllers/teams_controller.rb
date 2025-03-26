53ce664e2ad538906c224ffa4dd9ddd93349101d
Paul Crossley <pauljoncrossley@gmail.com>
2025-03-26 07:00:50 +0000

    Team descriptions

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -62,7 +62,7 @@ class TeamsController < ApplicationController
   private
 
     def team_params
-      params.require(:team).permit(:name)
+      params.require(:team).permit(:name, :description)
     rescue ActionController::ParameterMissing => e
       Rails.logger.error "Required parameter missing: #{e.message}"
       render json: { error: 'Required parameter missing' }, status: :bad_request
